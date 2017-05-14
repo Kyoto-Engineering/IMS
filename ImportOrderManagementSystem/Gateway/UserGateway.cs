499c43d227c417fa96c7a07ea8851e1ed2066203
Shimul <shimul39ju@gmail.com>
2017-05-14 11:37:31 +0600

    Initial Commit

---
+++ ImportOrderManagementSystem/Gateway/UserGateway.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImportOrderManagementSystem.DAL;
+using ImportOrderManagementSystem.DbGateway;
+
+namespace ImportOrderManagementSystem.Gateway
+{
+  public   class UserGateway:ConnectionGateway
+    {
+        private SqlCommand cmd;
+
+        //public int SaveUser(User nUser)
+        //{
+        //    connection.Open();
+        //    string insertquery = " insert into Registration(UserName,UserType,Password,Name,Designation,Department,ContactNo) Values('" + nUser.UserName + "','" + nUser.UserType + "','" + nUser.Password + "','" + nUser.Name + "','" + nUser.Designation + "','" + nUser.Department + "','" + nUser.ContactNumber + "')";
+        //    cmd = new SqlCommand(insertquery, connection);
+        //    int affectedrows = cmd.ExecuteNonQuery();
+        //    connection.Close();
+        //    return affectedrows;
+        //}
+    }
+}
---
+++ ImportOrderManagementSystem/Manager/UserManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImportOrderManagementSystem.DAL;
+using ImportOrderManagementSystem.Gateway;
+
+namespace ImportOrderManagementSystem.Manager
+{
+   public  class UserManager
+    {
+        UserGateway aGateway;
+        //public int SaveUser(User nUser)
+        //{
+        //    aGateway = new UserGateway();
+        //    return aGateway.SaveUser(nUser);
+        //}
+    }
+}
