5b14ce26588bf9dfac33d732a2868645b3a499f0
kowsar11035 <kowsar.jony11035@gmail.com>
2017-06-18 21:33:31 +0600

    add ProductAddedEXWPrice

---
+++ ImportOrderManagementSystem/UI/ProductAddedEXWPrice.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace ImportOrderManagementSystem.UI
+{
+    partial class ProductAddedEXWPrice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridViewk = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PricetextBox = new System.Windows.Forms.TextBox();
+            this.Savebutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewk)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridViewk);
+            this.groupBox2.Location = new System.Drawing.Point(318, 49);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(628, 247);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.UseCompatibleTextRendering = true;
+            // 
+            // dataGridViewk
+            // 
+            this.dataGridViewk.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewk.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Blue;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewk.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridViewk.Location = new System.Drawing.Point(6, 19);
+            this.dataGridViewk.Name = "dataGridViewk";
+            this.dataGridViewk.Size = new System.Drawing.Size(609, 211);
+            this.dataGridViewk.TabIndex = 121;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Product Id";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Product Generic Description";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 180;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Item Description";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 160;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Item Code";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 120;
+            // 
+            // PricetextBox
+            // 
+            this.PricetextBox.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PricetextBox.Location = new System.Drawing.Point(130, 115);
+            this.PricetextBox.Name = "PricetextBox";
+            this.PricetextBox.Size = new System.Drawing.Size(154, 26);
+            this.PricetextBox.TabIndex = 5;
+            // 
+            // Savebutton
+            // 
+            this.Savebutton.Location = new System.Drawing.Point(170, 164);
+            this.Savebutton.Name = "Savebutton";
+            this.Savebutton.Size = new System.Drawing.Size(75, 36);
+            this.Savebutton.TabIndex = 6;
+            this.Savebutton.Text = "Save";
+            this.Savebutton.UseVisualStyleBackColor = true;
+            this.Savebutton.Click += new System.EventHandler(this.Savebutton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(65, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 19);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Price";
+            // 
+            // ProductAddedEXWPrice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 613);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Savebutton);
+            this.Controls.Add(this.PricetextBox);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "ProductAddedEXWPrice";
+            this.Text = "ProductAddedEXWPrice";
+            this.Load += new System.EventHandler(this.ProductAddedEXWPrice_Load);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewk)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridViewk;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.TextBox PricetextBox;
+        private System.Windows.Forms.Button Savebutton;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ ImportOrderManagementSystem/UI/ProductAddedEXWPrice.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ImportOrderManagementSystem.DbGateway;
+
+namespace ImportOrderManagementSystem.UI
+{
+    public partial class ProductAddedEXWPrice : Form
+    {
+        SqlConnection _con;
+        SqlCommand _cmd;
+        ConnectionString _cs = new ConnectionString();
+        SqlDataReader rdr;
+        public ProductAddedEXWPrice()
+        {
+            InitializeComponent();
+        }
+
+        private void ProductAddedEXWPrice_Load(object sender, EventArgs e)
+        {
+            GridLoad();
+        }
+
+        private void GridLoad()
+        {
+            try
+            {
+
+                _con = new SqlConnection(_cs.DBConn);
+                _con.Open();
+                //cmd = new SqlCommand("SELECT RTRIM(ProductListSummary.Sl),RTRIM(ProductListSummary.ProductGenericDescription),RTRIM(ProductListSummary.ItemDescription),RTRIM(ProductListSummary.ItemCode),RTRIM(MasterStocks.MQuantity),RTRIM(MasterStocks.UnitPrice) from ProductListSummary,MasterStocks where MasterStocks.Sl=ProductListSummary.Sl order by MasterStocks.Sl desc", con);
+                _cmd = new SqlCommand(
+                    "SELECT ProductListSummary.Sl, ProductListSummary.ProductGenericDescription, ProductListSummary.ItemDescription, ProductListSummary.ItemCode FROM ProductListSummary LEFT JOIN EXWPrice ON ProductListSummary.Sl = EXWPrice.Sl where ProductListSummary.Sl not in(select EXWPrice.Sl from EXWPrice)", _con);
+                rdr = _cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                dataGridViewk.Rows.Clear();
+                while (rdr.Read() == true)
+                {
+                    dataGridViewk.Rows.Add(rdr[0], rdr[1], rdr[2], rdr[3]);
+                }
+
+                _con.Close();
+            
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void Savebutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                    _con = new SqlConnection(_cs.DBConn);
+                    string cd = "INSERT INTO EXWPrice (Price,Sl) VALUES (@d1,@d2)";
+                    _cmd = new SqlCommand(cd, _con);
+                    _cmd.Parameters.AddWithValue("@d1", PricetextBox.Text);
+                    _cmd.Parameters.AddWithValue("d2", dataGridViewk.Rows[0].Cells[0].Value);                   
+                    _con.Open();
+                    _cmd.ExecuteNonQuery();
+                    _con.Close();
+
+                
+                
+                MessageBox.Show("Saved Successfully.", "Record", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                
+                dataGridViewk.Enabled = false;
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
