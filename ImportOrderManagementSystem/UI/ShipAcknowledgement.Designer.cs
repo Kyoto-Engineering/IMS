d98129e56c3883f8efa1be5ebcfbf24b9b2ae9d2
Ikramunnesa <ikraafi@gmail.com>
2017-06-19 12:25:29 +0600

    modify design ImportOrderManagementSystem/UI/ShipAcknowledgement.Designer.cs

--- ImportOrderManagementSystem/UI/ShipAcknowledgement.Designer.cs
+++ ImportOrderManagementSystem/UI/ShipAcknowledgement.Designer.cs
@@ -32,19 +32,19 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ShipAcknowledgement));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SupplierComboBox = new System.Windows.Forms.ComboBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.button3 = new System.Windows.Forms.Button();
+            this.DoneButton = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.ProductDesTextBox = new System.Windows.Forms.TextBox();
+            this.ProductNameTextBox = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.RemoveButton = new System.Windows.Forms.Button();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.ShipingQtyTextBox = new System.Windows.Forms.TextBox();
+            this.ProductCodeTextBox = new System.Windows.Forms.TextBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -77,7 +77,7 @@
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.SupplierComboBox);
             this.groupBox1.Location = new System.Drawing.Point(13, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(414, 143);
@@ -94,28 +94,28 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Select Supplier";
             // 
-            // comboBox1
+            // SupplierComboBox
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(214, 20);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(141, 21);
-            this.comboBox1.TabIndex = 1;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.SupplierComboBox.FormattingEnabled = true;
+            this.SupplierComboBox.Location = new System.Drawing.Point(214, 20);
+            this.SupplierComboBox.Name = "SupplierComboBox";
+            this.SupplierComboBox.Size = new System.Drawing.Size(141, 21);
+            this.SupplierComboBox.TabIndex = 1;
+            this.SupplierComboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.DoneButton);
             this.groupBox2.Controls.Add(this.label5);
             this.groupBox2.Controls.Add(this.label6);
-            this.groupBox2.Controls.Add(this.textBox3);
-            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.ProductDesTextBox);
+            this.groupBox2.Controls.Add(this.ProductNameTextBox);
             this.groupBox2.Controls.Add(this.label4);
             this.groupBox2.Controls.Add(this.label3);
-            this.groupBox2.Controls.Add(this.button2);
-            this.groupBox2.Controls.Add(this.button1);
-            this.groupBox2.Controls.Add(this.textBox2);
-            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.RemoveButton);
+            this.groupBox2.Controls.Add(this.AddButton);
+            this.groupBox2.Controls.Add(this.ShipingQtyTextBox);
+            this.groupBox2.Controls.Add(this.ProductCodeTextBox);
             this.groupBox2.Location = new System.Drawing.Point(13, 162);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(414, 165);
@@ -123,15 +123,15 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Product Info";
             // 
-            // button3
+            // DoneButton
             // 
-            this.button3.Location = new System.Drawing.Point(333, 123);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "Done";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.DoneButton.Location = new System.Drawing.Point(333, 123);
+            this.DoneButton.Name = "DoneButton";
+            this.DoneButton.Size = new System.Drawing.Size(75, 23);
+            this.DoneButton.TabIndex = 7;
+            this.DoneButton.Text = "Done";
+            this.DoneButton.UseVisualStyleBackColor = true;
+            this.DoneButton.Click += new System.EventHandler(this.button3_Click);
             // 
             // label5
             // 
@@ -151,21 +151,21 @@
             this.label6.TabIndex = 9;
             this.label6.Text = "Product Name";
             // 
-            // textBox3
+            // ProductDesTextBox
             // 
-            this.textBox3.Location = new System.Drawing.Point(108, 48);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.ReadOnly = true;
-            this.textBox3.Size = new System.Drawing.Size(191, 20);
-            this.textBox3.TabIndex = 2;
+            this.ProductDesTextBox.Location = new System.Drawing.Point(108, 48);
+            this.ProductDesTextBox.Name = "ProductDesTextBox";
+            this.ProductDesTextBox.ReadOnly = true;
+            this.ProductDesTextBox.Size = new System.Drawing.Size(191, 20);
+            this.ProductDesTextBox.TabIndex = 2;
             // 
-            // textBox4
+            // ProductNameTextBox
             // 
-            this.textBox4.Location = new System.Drawing.Point(108, 22);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.ReadOnly = true;
-            this.textBox4.Size = new System.Drawing.Size(191, 20);
-            this.textBox4.TabIndex = 1;
+            this.ProductNameTextBox.Location = new System.Drawing.Point(108, 22);
+            this.ProductNameTextBox.Name = "ProductNameTextBox";
+            this.ProductNameTextBox.ReadOnly = true;
+            this.ProductNameTextBox.Size = new System.Drawing.Size(191, 20);
+            this.ProductNameTextBox.TabIndex = 1;
             // 
             // label4
             // 
@@ -185,46 +185,47 @@
             this.label3.TabIndex = 5;
             this.label3.Text = "Product Code";
             // 
-            // button2
+            // RemoveButton
             // 
-            this.button2.Location = new System.Drawing.Point(333, 46);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "Remove";
-            this.button2.UseVisualStyleBackColor = true;
+            this.RemoveButton.Location = new System.Drawing.Point(333, 46);
+            this.RemoveButton.Name = "RemoveButton";
+            this.RemoveButton.Size = new System.Drawing.Size(75, 23);
+            this.RemoveButton.TabIndex = 6;
+            this.RemoveButton.Text = "Remove";
+            this.RemoveButton.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // AddButton
             // 
-            this.button1.Location = new System.Drawing.Point(333, 20);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Add";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.AddButton.Location = new System.Drawing.Point(333, 20);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(75, 23);
+            this.AddButton.TabIndex = 5;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox2
+            // ShipingQtyTextBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(110, 111);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 4;
+            this.ShipingQtyTextBox.Location = new System.Drawing.Point(110, 111);
+            this.ShipingQtyTextBox.Name = "ShipingQtyTextBox";
+            this.ShipingQtyTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ShipingQtyTextBox.TabIndex = 4;
+            this.ShipingQtyTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ShipingQtyTextBox_KeyDown);
             // 
-            // textBox1
+            // ProductCodeTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(108, 74);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(191, 20);
-            this.textBox1.TabIndex = 3;
+            this.ProductCodeTextBox.Location = new System.Drawing.Point(108, 74);
+            this.ProductCodeTextBox.Name = "ProductCodeTextBox";
+            this.ProductCodeTextBox.ReadOnly = true;
+            this.ProductCodeTextBox.Size = new System.Drawing.Size(191, 20);
+            this.ProductCodeTextBox.TabIndex = 3;
             // 
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.dataGridView1);
-            this.groupBox3.Location = new System.Drawing.Point(433, 55);
+            this.groupBox3.Location = new System.Drawing.Point(450, 12);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(717, 171);
+            this.groupBox3.Size = new System.Drawing.Size(557, 219);
             this.groupBox3.TabIndex = 2;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Ordered Product";
@@ -249,7 +250,7 @@
             this.dataGridView1.ReadOnly = true;
             dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
             this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle1;
-            this.dataGridView1.Size = new System.Drawing.Size(711, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(544, 198);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
             // 
@@ -317,9 +318,9 @@
             // groupBox4
             // 
             this.groupBox4.Controls.Add(this.listView1);
-            this.groupBox4.Location = new System.Drawing.Point(433, 232);
+            this.groupBox4.Location = new System.Drawing.Point(451, 246);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(717, 100);
+            this.groupBox4.Size = new System.Drawing.Size(604, 210);
             this.groupBox4.TabIndex = 3;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "groupBox4";
@@ -338,7 +339,7 @@
             this.columnHeader9});
             this.listView1.Location = new System.Drawing.Point(7, 20);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(704, 74);
+            this.listView1.Size = new System.Drawing.Size(591, 184);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -367,12 +368,12 @@
             // 
             this.columnHeader5.Text = "Product Code";
             this.columnHeader5.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.columnHeader5.Width = 180;
+            this.columnHeader5.Width = 84;
             // 
             // columnHeader6
             // 
             this.columnHeader6.Text = "Product Description";
-            this.columnHeader6.Width = 200;
+            this.columnHeader6.Width = 203;
             // 
             // columnHeader7
             // 
@@ -394,7 +395,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1162, 339);
+            this.ClientSize = new System.Drawing.Size(1068, 481);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
@@ -421,25 +422,25 @@
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.GroupBox groupBox4;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox SupplierComboBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button RemoveButton;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.TextBox ShipingQtyTextBox;
+        private System.Windows.Forms.TextBox ProductCodeTextBox;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox ProductDesTextBox;
+        private System.Windows.Forms.TextBox ProductNameTextBox;
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.ColumnHeader columnHeader3;
         private System.Windows.Forms.ColumnHeader columnHeader4;
         private System.Windows.Forms.ColumnHeader columnHeader5;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button DoneButton;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
