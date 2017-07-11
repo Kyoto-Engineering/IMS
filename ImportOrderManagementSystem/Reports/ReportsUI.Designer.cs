852171a461aaa41d8914ced76591b245b09fe700
Ikramunnesa <ikraafi@gmail.com>
2017-07-11 11:22:03 +0600

    design modified

--- ImportOrderManagementSystem/Reports/ReportsUI.Designer.cs
+++ ImportOrderManagementSystem/Reports/ReportsUI.Designer.cs
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.CloseButton = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.ShipOrdButton = new System.Windows.Forms.Button();
             this.ShipAckButton = new System.Windows.Forms.Button();
+            this.ShipOrdButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -47,53 +47,63 @@
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             // 
-            // CloseButton
-            // 
-            this.CloseButton.Location = new System.Drawing.Point(509, 12);
-            this.CloseButton.Name = "CloseButton";
-            this.CloseButton.Size = new System.Drawing.Size(75, 23);
-            this.CloseButton.TabIndex = 1;
-            this.CloseButton.Text = "Close";
-            this.CloseButton.UseVisualStyleBackColor = true;
-            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(26, 30);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(126, 52);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "Import Order";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // ShipOrdButton
-            // 
-            this.ShipOrdButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ShipOrdButton.Location = new System.Drawing.Point(172, 30);
-            this.ShipOrdButton.Name = "ShipOrdButton";
-            this.ShipOrdButton.Size = new System.Drawing.Size(151, 52);
-            this.ShipOrdButton.TabIndex = 1;
-            this.ShipOrdButton.Text = "Shipment Order";
-            this.ShipOrdButton.UseVisualStyleBackColor = true;
-            this.ShipOrdButton.Click += new System.EventHandler(this.ShipOrdButton_Click);
-            // 
             // ShipAckButton
             // 
+            this.ShipAckButton.BackColor = System.Drawing.Color.CornflowerBlue;
             this.ShipAckButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ShipAckButton.ForeColor = System.Drawing.Color.Cornsilk;
             this.ShipAckButton.Location = new System.Drawing.Point(341, 30);
             this.ShipAckButton.Name = "ShipAckButton";
             this.ShipAckButton.Size = new System.Drawing.Size(166, 52);
             this.ShipAckButton.TabIndex = 2;
             this.ShipAckButton.Text = "Shipment Acknowledgement";
-            this.ShipAckButton.UseVisualStyleBackColor = true;
+            this.ShipAckButton.UseVisualStyleBackColor = false;
             this.ShipAckButton.Click += new System.EventHandler(this.ShipAckButton_Click);
+            // 
+            // ShipOrdButton
+            // 
+            this.ShipOrdButton.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ShipOrdButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ShipOrdButton.ForeColor = System.Drawing.Color.Cornsilk;
+            this.ShipOrdButton.Location = new System.Drawing.Point(172, 30);
+            this.ShipOrdButton.Name = "ShipOrdButton";
+            this.ShipOrdButton.Size = new System.Drawing.Size(151, 52);
+            this.ShipOrdButton.TabIndex = 1;
+            this.ShipOrdButton.Text = "Shipment Order";
+            this.ShipOrdButton.UseVisualStyleBackColor = false;
+            this.ShipOrdButton.Click += new System.EventHandler(this.ShipOrdButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Cornsilk;
+            this.button2.Location = new System.Drawing.Point(26, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 52);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Import Order";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.CloseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CloseButton.ForeColor = System.Drawing.Color.Crimson;
+            this.CloseButton.Location = new System.Drawing.Point(509, 12);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(75, 36);
+            this.CloseButton.TabIndex = 1;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = false;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
             // 
             // ReportsUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CadetBlue;
             this.ClientSize = new System.Drawing.Size(598, 407);
             this.Controls.Add(this.CloseButton);
             this.Controls.Add(this.groupBox1);
